ddd5d71ecdd8b999e5d3844e9a35be3ff82114a6
Balazs Gonter <gonter.balazs@gmail.com>
2023-09-27 12:00:08 +0200

    Extract new component and try out props

--- Part1/src/App.jsx
+++ Part1/src/App.jsx
@@ -1,17 +1,34 @@
+const Hello = (props) => {
+    console.log(props)
+    return (
+        <div>
+            <p>Hello {props.name}, you are {props.age} years old </p>
+        </div>
+    )
+}
+
 const App = () => {
 
+    // Console related variables
     const now = new Date()
     const a = 10
     const b = 20
 
     console.log('Hello from component')
     console.log(now, a + b)
+
+    // Trying out to use props from components
+    const name = 'Peter'
+    const age = 10
+
     return (
         <div>
-            <p>Hello world</p>
-            <p>
-                {a} plus {b} is {a + b}
-            </p>
+            <h1>Hello world / Greetings</h1>
+            <Hello name='George'/>
+            <Hello name='Something'/>
+            <Hello name='Maya' age={26 + 10} />
+            <Hello name={name} age={age} />
+            <Hello/>
         </div>
     )
 }
