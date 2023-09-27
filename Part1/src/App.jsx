86654451e332b574837c2ffcb6ae27b4df3409ed
Balazs Gonter <gonter.balazs@gmail.com>
2023-09-27 13:59:39 +0200

    Try out how to access objects and arrays in rendering

--- Part1/src/App.jsx
+++ Part1/src/App.jsx
@@ -21,6 +21,15 @@ const App = () => {
     const name = 'Peter'
     const age = 10
 
+    // Define objects as constant array without Component and use them directly as props
+    const friends = [
+        {name: 'John', age: 15},
+        {name: 'Jane', age: 21}
+    ]
+
+    // Arrays can be rendered unlik components
+    const friendArray = [ 'Peter', 'Maya']
+    
     return (
         <div>
             <h1>Hello world / Greetings</h1>
@@ -29,6 +38,9 @@ const App = () => {
             <Hello name='Maya' age={26 + 10} />
             <Hello name={name} age={age} />
             <Hello/>
+            <p>{friends[0].name} is {friends[0].age} years old</p>
+            <p>{friends[1].name} is {friends[1].age} years old</p>
+            <p>{friendArray[0]} and {friendArray[1]} are rendered properly</p>
         </div>
     )
 }
