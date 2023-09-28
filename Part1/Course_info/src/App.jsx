c03fc24882687e435de2c63afc39c91b1167ffdf
Balazs Gonter <gonter.balazs@gmail.com>
2023-09-28 15:48:45 +0200

    Exercise 2 - Introduce Part component

--- Part1/Course_info/src/App.jsx
+++ Part1/Course_info/src/App.jsx
@@ -6,9 +6,19 @@ const Header = (props) => {
     )
 }
 
+const Part = (props) => {
+    return (
+        <p>{props.part} {props.exercises}</p>
+    )
+}
+
 const Content = (props) => {
     return (
-        <p>{props.name} {props.exercise}</p>
+        <div>
+            <Part part={props.part1} exercises={props.exercises1}/>
+            <Part part={props.part2} exercises={props.exercises2}/>
+            <Part part={props.part3} exercises={props.exercises3}/>
+        </div>
     )
 }
 
@@ -30,9 +40,7 @@ const App = () => {
     return (
       <div>
         <Header course = {course}/>
-        <Content name = {part1} exercise = {exercises1}/>
-        <Content name = {part2} exercise = {exercises2}/>
-        <Content name = {part3} exercise = {exercises3}/>
+        <Content part1={part1} part2={part2} part3={part3} exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
         <Total exercises1 = {exercises1} exercises2 = {exercises2} exercises3 = {exercises3}/>
       </div>
     )
