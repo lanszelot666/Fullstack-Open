b257aeca6c0046c7c4b98315c864e0dc99965f0b
Balazs Gonter <gonter.balazs@gmail.com>
2023-09-28 15:28:10 +0200

    Exercise 1 - Refactor App.jsx to multiple components

--- Part1/Course_info/src/App.jsx
+++ Part1/Course_info/src/App.jsx
@@ -1,3 +1,23 @@
+const Header = (props) => {
+    return (
+        <div>
+            <h1>{props.course}</h1>
+        </div>
+    )
+}
+
+const Content = (props) => {
+    return (
+        <p>{props.name} {props.exercise}</p>
+    )
+}
+
+const Total = (props) => {
+    return (
+        <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+    )
+}
+
 const App = () => {
     const course = 'Half Stack application development'
     const part1 = 'Fundamentals of React'
@@ -9,17 +29,11 @@ const App = () => {
   
     return (
       <div>
-        <h1>{course}</h1>
-        <p>
-          {part1} {exercises1}
-        </p>
-        <p>
-          {part2} {exercises2}
-        </p>
-        <p>
-          {part3} {exercises3}
-        </p>
-        <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+        <Header course = {course}/>
+        <Content name = {part1} exercise = {exercises1}/>
+        <Content name = {part2} exercise = {exercises2}/>
+        <Content name = {part3} exercise = {exercises3}/>
+        <Total exercises1 = {exercises1} exercises2 = {exercises2} exercises3 = {exercises3}/>
       </div>
     )
   }
