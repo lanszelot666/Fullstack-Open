29b84a8197074edc153a38ad52f119733981665c
Balazs Gonter <gonter.balazs@gmail.com>
2023-10-16 15:14:23 +0200

    Complex states

--- Part1/Base_tutorial/src/App.jsx
+++ Part1/Base_tutorial/src/App.jsx
@@ -1,47 +1,35 @@
 import { useState } from 'react'
 
-const Display = ({ counter }) => <div>{counter}</div>
-
-const Button = ({ handleClick, text }) => <button onClick={handleClick}>{text}</button>
-
 const App = () => {
-  const [ counter, setCounter ] = useState(0)
-  console.log('rendering with counter value', counter)
 
-  // Usually defining event handlers within JSX-templates is not a good idea.
-  const increaseByOne = () =>  {
-    console.log('increasing, value before', counter)
-    setCounter(counter + 1)
-  }
+    const [clicks, setClicks] = useState({
+      left: 0, right: 0
+    })
+  
+    const handleLeftClick = () => {
+        const newClicks = { 
+            ...clicks, 
+            left: clicks.left + 1 
+        }
+        setClicks(newClicks)
+    }
 
-  const decreaseByOne = () => {
-    console.log('decreasing, value before', counter)
-    setCounter(counter - 1)
-  }
-
-  const setToZero = () => {
-    console.log('resetting to zero, value before', counter)
-    setCounter(0)
-  }
-
-  return (
-    <div>
-        <Display counter={counter}/>
-        <Button
-            handleClick={increaseByOne}
-            text='plus'
-        />
-        <Button
-            handleClick={setToZero}
-            text='zero'
-        />     
-        <Button
-            handleClick={decreaseByOne}
-            text='minus'
-        />  
-    </div>
-    
-  )
+    const handleRightClick = () => {
+        const newClicks = { 
+            ...clicks, 
+            right: clicks.right + 1 
+        }
+        setClicks(newClicks)
+    }
+  
+    return (
+      <div>
+        {clicks.left}
+        <button onClick={handleLeftClick}>left</button>
+        <button onClick={handleRightClick}>right</button>
+        {clicks.right}
+      </div>
+    )
 }
 
 export default App
