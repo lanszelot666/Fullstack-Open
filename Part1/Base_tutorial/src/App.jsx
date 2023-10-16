e0ec4da9e58ee0a09b899f276b27aa0503090f13
Balazs Gonter <gonter.balazs@gmail.com>
2023-10-16 15:06:17 +0200

    Change states, pass event handlers, simplify code

--- Part1/Base_tutorial/src/App.jsx
+++ Part1/Base_tutorial/src/App.jsx
@@ -1,22 +1,44 @@
 import { useState } from 'react'
 
+const Display = ({ counter }) => <div>{counter}</div>
+
+const Button = ({ handleClick, text }) => <button onClick={handleClick}>{text}</button>
+
 const App = () => {
   const [ counter, setCounter ] = useState(0)
+  console.log('rendering with counter value', counter)
 
   // Usually defining event handlers within JSX-templates is not a good idea.
-  const increaseByOne = () => setCounter(counter + 1)
-  
-  const setToZero = () => setCounter(0)
+  const increaseByOne = () =>  {
+    console.log('increasing, value before', counter)
+    setCounter(counter + 1)
+  }
+
+  const decreaseByOne = () => {
+    console.log('decreasing, value before', counter)
+    setCounter(counter - 1)
+  }
+
+  const setToZero = () => {
+    console.log('resetting to zero, value before', counter)
+    setCounter(0)
+  }
 
   return (
     <div>
-        <div>{counter}</div>
-        <button onClick={increaseByOne}>
-            plus
-        </button>
-        <button onClick={setToZero}> 
-            zero
-      </button>
+        <Display counter={counter}/>
+        <Button
+            handleClick={increaseByOne}
+            text='plus'
+        />
+        <Button
+            handleClick={setToZero}
+            text='zero'
+        />     
+        <Button
+            handleClick={decreaseByOne}
+            text='minus'
+        />  
     </div>
     
   )
