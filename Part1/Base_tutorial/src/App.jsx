db025b009679e7620761c2c0eabcfe033c2041ee
Balazs Gonter <gonter.balazs@gmail.com>
2023-10-16 14:36:06 +0200

    Component states
    * usestate initializes the state of counter, then setTimeout is used to
     update the state

--- Part1/Base_tutorial/src/App.jsx
+++ Part1/Base_tutorial/src/App.jsx
@@ -1,58 +1,18 @@
-const Hello = ({ name, age }) => {
+import { useState } from 'react'
 
-    const bornYear = () => new Date().getFullYear() - age
+const App = () => {
+  const [ counter, setCounter ] = useState(0)
 
-    const bornYear2 = () => {
-        const yearNow = new Date().getFullYear()
-        return yearNow - age
-    }
+  setTimeout(
+    () => setCounter(counter + 1),
+    1000
+  )
 
-    console.log(name, age)
-    return (
-        <div>
-            <p>Hello {name}, you are {age} years old </p>
-            <p>So you were probably born in {bornYear()} or in {bornYear2()}</p>
-        </div>
-    )
-}
+  console.log('rendering...', counter)
 
-const App = (props) => {
-
-    const {counter} = props
-
-    // Console related variables
-    const now = new Date()
-    const a = 10
-    const b = 20
-
-    console.log('Hello from component')
-    console.log(now, a + b)
-
-    // Trying out to use props from components
-    const name = 'Peter'
-    const age = 10
-
-    // Define objects as constant array without Component and use them directly as props
-    const friends = [
-        {name: 'John', age: 15},
-        {name: 'Jane', age: 21}
-    ]
-
-    // Arrays can be rendered unlik components
-    const friendArray = [ 'Peter', 'Maya']
-    
-    return (
-        <div>
-            <h1>Hello world / Greetings</h1>
-            <Hello name='Maya' age={26 + 10} />
-            <Hello name={name} age={age} />
-            <p>{friends[0].name} is {friends[0].age} years old</p>
-            <p>{friends[1].name} is {friends[1].age} years old</p>
-            <p>{friendArray[0]} and {friendArray[1]} are rendered properly</p>
-            <h1>Implementation of a counter</h1>
-            <div>{counter}</div>
-        </div>
-    )
+  return (
+    <div>{counter}</div>
+  )
 }
 
 export default App
--- Part1/Base_tutorial/src/main.jsx
+++ Part1/Base_tutorial/src/main.jsx
@@ -1,15 +1,5 @@
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 
-let counter = 1
+import App from './App'
 
-const refresh = () => {
-    ReactDOM.createRoot(document.getElementById('root')).render(
-        <App counter={counter} />
-    )
-}
-
-setInterval(() => {
-    refresh()
-    counter += 1
-}, 1000)
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
