3315cc02b74649db08a7235a99636d6437f7614a
Balazs Gonter <gonter.balazs@gmail.com>
2023-11-06 17:11:44 +0100

    Function that returns a function as event handler

--- Part1/Base_tutorial/src/App.jsx
+++ Part1/Base_tutorial/src/App.jsx
@@ -1,53 +1,60 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 const History = (props) => {
-    if (props.allClicks.length === 0) {
-      return (
-        <div>
-          the app is used by pressing the buttons
-        </div>
-      )
-    }
-    return (
-      <div>
-        button press history: {props.allClicks.join(' ')}
-      </div>
-    )
-}
+  if (props.allClicks.length === 0) {
+    return <div>the app is used by pressing the buttons</div>;
+  }
+  return <div>button press history: {props.allClicks.join(" ")}</div>;
+};
 
 const Button = ({ handleClick, text }) => (
-    <button onClick={handleClick}>
-      {text}
-    </button>
-)
+  <button onClick={handleClick}>{text}</button>
+);
 
 const App = () => {
+  const [left, setLeft] = useState(0);
+  const [right, setRight] = useState(0);
+  const [allClicks, setAll] = useState([]);
+  const [value, setValue] = useState(10);
 
-    const [left, setLeft] = useState(0)
-    const [right, setRight] = useState(0)
-    const [allClicks, setAll] = useState([])
-  
-    const handleLeftClick = () => {
-        setAll(allClicks.concat('L'))
-        const updatedLeft = left + 1
-        setLeft(updatedLeft)
-    }
-    
-    const handleRightClick = () => {
-        setAll(allClicks.concat('R'))
-        const updatedRight = right + 1
-        setRight(updatedRight)
-    }
-  
-    return (
-      <div>
-        {left}
-        <Button handleClick={handleLeftClick} text='left' />
-        <Button handleClick={handleRightClick} text='right' />      
-        {right}
-        <History allClicks={allClicks} />
-      </div>
-    )
-}
+  const setToValue = (newValue) => () => {
+    console.log("value now", newValue); // print the new value to console
+    setValue(newValue);
+  };
 
-export default App
+  const handleLeftClick = () => {
+    setAll(allClicks.concat("L"));
+    const updatedLeft = left + 1;
+    setLeft(updatedLeft);
+  };
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat("R"));
+    const updatedRight = right + 1;
+    setRight(updatedRight);
+  };
+
+  const hello = (who) => () => {
+    console.log("hello", who);
+  };
+
+  return (
+    <div>
+      {left}
+      <Button handleClick={handleLeftClick} text="left" />
+      <Button handleClick={handleRightClick} text="right" />
+      {right}
+      <History allClicks={allClicks} />
+      <p></p>
+      <button onClick={hello("world")}>button</button>
+      <button onClick={hello("react")}>button</button>
+      <button onClick={hello("function")}>button</button> <p></p>
+      <div>{value}</div>
+      <Button handleClick={setToValue(1000)} text="thousand" />
+      <Button handleClick={setToValue(0)} text="reset" />
+      <Button handleClick={setToValue(value + 1)} text="increment" />
+    </div>
+  );
+};
+
+export default App;
