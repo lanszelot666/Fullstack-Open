a86b5cb0e6dc1ed2b84f5931307a3c1dec53a5fd
Balazs Gonter <gonter.balazs@gmail.com>
2023-11-24 16:09:33 +0100

    Exercise 12 - add anecdotes
    * Random number generation with array

--- Part1/Unicafe/src/App.jsx
+++ Part1/Unicafe/src/App.jsx
@@ -26,7 +26,10 @@ const Statistics = ({ good, bad, neutral }) => {
           <StatisticLine text="bad" value={bad} />
           <StatisticLine text="all" value={calculateAll()} />
           <StatisticLine text="average" value={calculateAverage().toFixed(2)} />
-          <StatisticLine text="postive" value={calculatePositive().toFixed(2)}/>
+          <StatisticLine
+            text="postive"
+            value={calculatePositive().toFixed(2)}
+          />
         </tbody>
       </table>
     );
@@ -49,10 +52,31 @@ const StatisticLine = ({ text, value }) => {
 };
 
 const App = () => {
-  // save clicks of each button to its own state
+  const anecdotes = [
+    "If it hurts, do it more often.",
+    "Adding manpower to a late software project makes it later!",
+    "The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    "Premature optimization is the root of all evil.",
+    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
+    "The only way to go fast, is to go well.",
+  ];
+
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [selected, setSelected] = useState(0);
+
+  const handleGoodFeedback = () => handleFeedback("good");
+  const handleNeutralFeedback = () => handleFeedback("neutral");
+  const handleBadFeedback = () => handleFeedback("bad");
+
+  const handleAnecdoteButton = () => {
+    const rand = Math.floor(Math.random() * 8);
+    setSelected(rand);
+    console.log("Random generated number: ", rand);
+  };
 
   const handleFeedback = (feedbackType) => {
     const updatedGood = feedbackType === "good" ? good + 1 : good;
@@ -64,18 +88,17 @@ const App = () => {
     setBad(updatedBad);
   };
 
-  const handleGoodFeedback = () => handleFeedback("good");
-  const handleNeutralFeedback = () => handleFeedback("neutral");
-  const handleBadFeedback = () => handleFeedback("bad");
-
   return (
     <div>
+      <h3>{anecdotes[selected]}</h3>
+      <Button handleClick={handleAnecdoteButton} text="next anecdote" />
       <Header title="give feedback" />
-      <Button handleClick={handleGoodFeedback} text="good"></Button>
-      <Button handleClick={handleNeutralFeedback} text="neutral"></Button>
-      <Button handleClick={handleBadFeedback} text="bad"></Button>
+      <Button handleClick={handleGoodFeedback} text="good" />
+      <Button handleClick={handleNeutralFeedback} text="neutral" />
+      <Button handleClick={handleBadFeedback} text="bad" />
       <Header title="statistics" />
-      <Statistics good={good} neutral={neutral} bad={bad}></Statistics>
+      <Statistics good={good} neutral={neutral} bad={bad} />
+      <br />
     </div>
   );
 };
