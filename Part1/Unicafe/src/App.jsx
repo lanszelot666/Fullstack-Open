a548c2bf5994893d7d3ac5b07251ef35cc1f9118
Balazs Gonter <gonter.balazs@gmail.com>
2023-11-24 14:59:18 +0100

    Exercise 7 - include new states and refactor handlers
    * New states: all, average, positive

--- Part1/Unicafe/src/App.jsx
+++ Part1/Unicafe/src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 
-
 const Header = (props) => {
   return (
     <div>
@@ -18,34 +17,47 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [all, setAll] = useState(0);
+  const [average, setAverage] = useState(0);
+  const [positive, setPositive] = useState(0);
 
+  const handleFeedback = (feedbackType) => {
+    const updatedGood = feedbackType === "good" ? good + 1 : good;
+    const updatedNeutral = feedbackType === "neutral" ? neutral + 1 : neutral;
+    const updatedBad = feedbackType === "bad" ? bad + 1 : bad;
+    const updatedAll = all + 1;
 
-  const increaseGoodFeedback = () => {
-    const updatedGood = good + 1;
     setGood(updatedGood);
-  };
-  
-  const increaseNeutralFeedback = () => {
-    const updatedNeutral = neutral + 1;
     setNeutral(updatedNeutral);
-  };
-  
-  const increaseBadFeedback = () => {
-    const updatedBad = bad + 1;
     setBad(updatedBad);
+    setAll(updatedAll);
+
+    const newAverage = (updatedGood - updatedBad) / updatedAll;
+    const newPositive = (updatedGood / updatedAll) * 100;
+
+    setAverage(newAverage);
+    setPositive(newPositive);
   };
+
+  const handleGoodFeedback = () => handleFeedback("good");
+  const handleNeutralFeedback = () => handleFeedback("neutral");
+  const hadnleBadFeedback = () => handleFeedback("bad");
+
 
   return (
     <div>
       <Header title="give feedback" />
-      <Button handleClick={increaseGoodFeedback} text="good"></Button>
-      <Button handleClick={increaseNeutralFeedback} text="neutral"></Button>
-      <Button handleClick={increaseBadFeedback} text="bad"></Button>
+      <Button handleClick={handleGoodFeedback} text="good"></Button>
+      <Button handleClick={handleNeutralFeedback} text="neutral"></Button>
+      <Button handleClick={hadnleBadFeedback} text="bad"></Button>
       <Header title="statistics" />
       <div>
         <p>good {good}</p>
         <p>neutral {neutral}</p>
         <p>bad {bad}</p>
+        <p>all {all}</p>
+        <p>average {average.toFixed(2)}</p>
+        <p>postive {positive.toFixed(2)} %</p>
       </div>
     </div>
   );
