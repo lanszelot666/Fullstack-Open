561924999a0adfc6943306b5bca5f2317b33bb55
Balazs Gonter <gonter.balazs@gmail.com>
2023-11-24 15:08:45 +0100

    Exercise 7 - get rid of unnecessary states
    * Simplify code

--- Part1/Unicafe/src/App.jsx
+++ Part1/Unicafe/src/App.jsx
@@ -17,31 +17,24 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-  const [all, setAll] = useState(0);
-  const [average, setAverage] = useState(0);
-  const [positive, setPositive] = useState(0);
 
   const handleFeedback = (feedbackType) => {
     const updatedGood = feedbackType === "good" ? good + 1 : good;
     const updatedNeutral = feedbackType === "neutral" ? neutral + 1 : neutral;
     const updatedBad = feedbackType === "bad" ? bad + 1 : bad;
-    const updatedAll = all + 1;
 
     setGood(updatedGood);
     setNeutral(updatedNeutral);
     setBad(updatedBad);
-    setAll(updatedAll);
-
-    const newAverage = (updatedGood - updatedBad) / updatedAll;
-    const newPositive = (updatedGood / updatedAll) * 100;
-
-    setAverage(newAverage);
-    setPositive(newPositive);
   };
 
   const handleGoodFeedback = () => handleFeedback("good");
   const handleNeutralFeedback = () => handleFeedback("neutral");
-  const hadnleBadFeedback = () => handleFeedback("bad");
+  const handleBadFeedback = () => handleFeedback("bad");
+
+  const calculateAll = () => good + neutral + bad;
+  const calculateAverage = () => (good - bad) / (good + neutral + bad);
+  const calculatePositive = () => good / (good + neutral + bad) * 100;
 
 
   return (
@@ -49,15 +42,15 @@ const App = () => {
       <Header title="give feedback" />
       <Button handleClick={handleGoodFeedback} text="good"></Button>
       <Button handleClick={handleNeutralFeedback} text="neutral"></Button>
-      <Button handleClick={hadnleBadFeedback} text="bad"></Button>
+      <Button handleClick={handleBadFeedback} text="bad"></Button>
       <Header title="statistics" />
       <div>
         <p>good {good}</p>
         <p>neutral {neutral}</p>
         <p>bad {bad}</p>
-        <p>all {all}</p>
-        <p>average {average.toFixed(2)}</p>
-        <p>postive {positive.toFixed(2)} %</p>
+        <p>all {calculateAll()}</p>
+        <p>average {calculateAverage().toFixed(2)}</p>
+        <p>postive {calculatePositive().toFixed(2)} %</p>
       </div>
     </div>
   );
