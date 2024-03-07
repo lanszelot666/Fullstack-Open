69832d73122570455dec5ddcdaea91d05be35d81
Balazs Gonter <gonter.balazs@gmail.com>
2024-03-07 16:23:28 +0100

    Clean-up - delete unused Total component

--- Course_info/src/App.jsx
+++ Course_info/src/App.jsx
@@ -1,16 +1,5 @@
 import Course from "./components/Course";
 
-const Total = (props) => {
-  return (
-    <p>
-      Number of exercises{" "}
-      {props.course.parts[0].exercises +
-        props.course.parts[1].exercises +
-        props.course.parts[2].exercises}
-    </p>
-  );
-};
-
 const App = () => {
   const courses = [
     {
