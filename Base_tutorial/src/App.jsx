820c8f3adf47a0575bc2d0f37543b6dd2fff9df1
Balazs Gonter <gonter.balazs@gmail.com>
2024-03-21 15:43:04 +0100

    Basics - implement form
    * Added onSubmit event and couple buttons
    * Controlled states

--- Base_tutorial/src/App.jsx
+++ Base_tutorial/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import Note from "./components/Note";
 
 const History = (props) => {
   if (props.allClicks.length === 0) {
@@ -11,11 +12,33 @@ const Button = ({ handleClick, text }) => (
   <button onClick={handleClick}>{text}</button>
 );
 
-const App = () => {
+const App = (props) => {
   const [left, setLeft] = useState(0);
   const [right, setRight] = useState(0);
   const [allClicks, setAll] = useState([]);
   const [value, setValue] = useState(10);
+  const [notes, setNotes] = useState(props.notes);
+  const [newNote, setNewNote] = useState("a new note...");
+  const [showAll, setShowAll] = useState(true);
+
+  const handleNoteChange = (event) => {
+    console.log(event.target.value);
+    setNewNote(event.target.value);
+  };
+
+  const addNote = (event) => {
+    event.preventDefault();
+    const noteObject = {
+      content: newNote,
+      important: Math.random() < 0.5,
+      id: notes.length + 1,
+    };
+
+    setNotes(notes.concat(noteObject));
+    setNewNote("");
+  };
+
+  const notesToShow = showAll ? notes : notes.filter((note) => note.important);
 
   const setToValue = (newValue) => () => {
     console.log("value now", newValue); // print the new value to console
@@ -40,6 +63,7 @@ const App = () => {
 
   return (
     <div>
+      <h1>Buttons and interactions</h1>
       {left}
       <Button handleClick={handleLeftClick} text="left" />
       <Button handleClick={handleRightClick} text="right" />
@@ -53,6 +77,21 @@ const App = () => {
       <Button handleClick={setToValue(1000)} text="thousand" />
       <Button handleClick={setToValue(0)} text="reset" />
       <Button handleClick={setToValue(value + 1)} text="increment" />
+      <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? "important" : "all"}
+        </button>
+      </div>
+      <ul>
+        {notesToShow.map((note) => (
+          <Note key={note.id} note={note} />
+        ))}
+      </ul>
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange} />
+        <button type="submit">save</button>
+      </form>
     </div>
   );
 };
