bb5086de6e266ed6a598ba176b9e062d36c29b38
Balazs Gonter <gonter.balazs@gmail.com>
2024-03-21 16:44:27 +0100

    Exercise 6 - Start implementing Phonebook program

---
+++ Phonebook/src/App.jsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import Person from "./components/Person";
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+
+  const handleNewName = (event) => {
+    console.log(event.target.value);
+    setNewName(event.target.value);
+  };
+
+  const addName = (event) => {
+    event.preventDefault();
+
+    const newPerson = {
+      name: newName,
+    };
+
+    console.log("persons: ", persons);
+    persons.push(newPerson);
+
+    setPersons(persons);
+    setNewName("");
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+          name: <input value={newName} onChange={handleNewName} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <div>
+        {persons.map((person) => (
+          <Person key={person.name} name={person.name} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ Phonebook/src/components/Person.jsx
@@ -0,0 +1,5 @@
+const Person = ({ name }) => {
+  return <div>{name}</div>;
+};
+
+export default Person;
