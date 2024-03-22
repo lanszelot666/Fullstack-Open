e3e4a2f6ebff1b5966fe2f62464cde91a6ae6514
Balazs Gonter <gonter.balazs@gmail.com>
2024-03-22 13:54:22 +0100

    Basics - Implement put, patch and post requests

--- Base_tutorial/src/App.jsx
+++ Base_tutorial/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
-import axios from "axios";
 import Note from "./components/Note";
+import noteService from "./services/notes";
 
 const History = (props) => {
   if (props.allClicks.length === 0) {
@@ -18,25 +18,18 @@ const App = (props) => {
   const [right, setRight] = useState(0);
   const [allClicks, setAll] = useState([]);
   const [value, setValue] = useState(10);
-  const [notes, setNotes] = useState(props.notes);
+  const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState("a new note...");
   const [showAll, setShowAll] = useState(true);
 
-  // Demo for fetching data from json-server
-  const [fetchedNotes, setFetchedNotes] = useState([]);
-
   // Fetch the data from the json-server /notes endpoint and log it out
-  const hook = () => {
-    console.log("effect");
-    axios.get("http://localhost:3001/notes").then((response) => {
-      console.log("promise fulfilled");
-      setFetchedNotes(response.data);
+  useEffect(() => {
+    noteService.getAll().then((initialNotes) => {
+      setNotes(initialNotes);
     });
-  };
+  }, []);
 
-  useEffect(hook, []);
-
-  console.log("render", fetchedNotes.length, "notes");
+  console.log("render", notes.length, "notes");
 
   const handleNoteChange = (event) => {
     console.log(event.target.value);
@@ -48,11 +41,28 @@ const App = (props) => {
     const noteObject = {
       content: newNote,
       important: Math.random() < 0.5,
-      id: notes.length + 1,
+      id: (notes.length + 1).toString(),
     };
 
-    setNotes(notes.concat(noteObject));
-    setNewNote("");
+    noteService.create(noteObject).then((returnedNote) => {
+      setNotes(notes.concat(returnedNote));
+      setNewNote("");
+    });
+  };
+
+  const toggleImportanceOf = (id) => {
+    const note = notes.find((n) => n.id === id);
+    const changedNote = { ...note, important: !note.important };
+
+    noteService
+      .update(id, changedNote)
+      .then((returnedNote) => {
+        setNotes(notes.map((note) => (note.id !== id ? note : returnedNote)));
+      })
+      .catch((error) => {
+        alert(`the note '${note.content}' was already deleted from server`);
+        setNotes(notes.filter((n) => n.id !== id));
+      });
   };
 
   const notesToShow = showAll ? notes : notes.filter((note) => note.important);
@@ -102,7 +112,11 @@ const App = (props) => {
       </div>
       <ul>
         {notesToShow.map((note) => (
-          <Note key={note.id} note={note} />
+          <Note
+            key={note.id}
+            note={note}
+            toggleImportance={() => toggleImportanceOf(note.id)}
+          />
         ))}
       </ul>
       <form onSubmit={addNote}>
