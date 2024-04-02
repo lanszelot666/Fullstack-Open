7ad4f4e3ce89a57f88f7f2e0fc51ffd9133f4743
Balazs Gonter <gonter.balazs@gmail.com>
2024-04-02 19:06:55 +0200

    Configure fly and deploy the backend

--- Backend_basics/index.js
+++ Backend_basics/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 
 let notes = [
   {
@@ -27,6 +28,7 @@ const requestLogger = (request, response, next) => {
   next();
 };
 
+app.use(cors());
 app.use(express.json());
 app.use(requestLogger);
 
@@ -90,7 +92,7 @@ app.post("/api/notes", (request, response) => {
 
 app.use(unknownEndpoint);
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
